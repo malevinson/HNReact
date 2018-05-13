97cc9efd8a381d113de46a4212fdc371d9414479
Matt Levinson <mlevinso@organic.com>
2018-05-13 12:55:01 -0700

    stories row 1 styling

--- src/components/Story.js
+++ src/components/Story.js
@@ -4,12 +4,13 @@ import './Story.css';
 const Story = ({ handleClickComments, name, time, rating, comments, number, source, url, handleClickStory }) => {
     return (
         <div className="story">
-            <span className="number">{number}</span>
-            <a target="_blank" href={url}>
-                {name}
-            </a>
-
-            <span className="source">{source}</span>
+            <div className="story-row">
+                <span className="number">{number}</span>
+                <a target="_blank" href={url}>
+                    {name}
+                </a>
+                <span className="source">{source}</span>
+            </div>
             <div className="rating-bars">{rating}</div>
             <div className="comment-bars">{comments}</div>
             <span className="time">{time}</span>
