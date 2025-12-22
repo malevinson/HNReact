9a3ca6672b3871f6b80cf01fd1faf0a756cf054f
Matt Levinson <matt.a.levinson@gmail.com>
2025-12-22 14:05:21 -0500

    add git commit message

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './containers/App';
 
-// Log git commit hash for deployment tracking
+// Log git commit hash and message for deployment tracking
 const gitCommitHash = process.env.REACT_APP_GIT_COMMIT_HASH || 'unknown';
+const gitCommitMessage = process.env.REACT_APP_GIT_COMMIT_MESSAGE || 'unknown';
 console.log(`Deployment commit hash: ${gitCommitHash}`);
+console.log(`Deployment commit message: ${gitCommitMessage}`);
 
 ReactDOM.render(
   <>
