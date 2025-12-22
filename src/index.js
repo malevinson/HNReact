58f1220175d804023650121f5e97789d11d5ed8f
Matt Levinson <matt.a.levinson@gmail.com>
2025-12-22 14:02:53 -0500

    add git hash

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './containers/App';
 
+// Log git commit hash for deployment tracking
+const gitCommitHash = process.env.REACT_APP_GIT_COMMIT_HASH || 'unknown';
+console.log(`Deployment commit hash: ${gitCommitHash}`);
+
 ReactDOM.render(
   <>
     <CssBaseline />
